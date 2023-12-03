442896f3bb5d84b0ffb14069a8478f0123c73a20
JCSnap <e0970167@u.nus.edu>
2023-12-03 23:43:53 +0800

    add day two second part

--- day-two/day-two-second.py
+++ day-two/day-two-second.py
@@ -1,9 +1,5 @@
 # Current bag: 12 red cubes, 13 green cubes, and 14 blue cubes
 
-max_red_count = 12
-max_green_count = 13
-max_blue_count = 14
-
 total = 0
 
 with open('day-two.in') as f:
@@ -12,10 +8,10 @@ with open('day-two.in') as f:
         # Eg. 4 blue, 8 green, 5 red; 14 red, 8 blue, 1 green; 11 red, 13 green would be 3 elements in the list
         records = line[line.find(":") + 1:].split(";")
         isValid = True
+        smallest_red_count = 0
+        smallest_green_count = 0
+        smallest_blue_count = 0
         for record in records:
-            red_count = 0
-            green_count = 0
-            blue_count = 0
             # Eg. 4 blue, 8 green, 5 red would be a list of 3 elements: "4 blue", "8 green", "5 red"
             colors = record.split(',')
             for color in colors:
@@ -24,19 +20,19 @@ with open('day-two.in') as f:
                 number = int(parts[0])
                 color_name = parts[1]
                 if color_name == "red":
-                    red_count += number
+                    if number > smallest_red_count:
+                        smallest_red_count = number
                 elif color_name == "green":
-                    green_count += number
+                    if number > smallest_green_count:
+                        smallest_green_count = number
                 elif color_name == "blue":
-                    blue_count += number
+                    if number > smallest_blue_count:
+                        smallest_blue_count = number
                 else:
                     isValid = False
                     break
-            if red_count > max_red_count or green_count > max_green_count or blue_count > max_blue_count:
-                isValid = False
-                break
+        power = smallest_red_count * smallest_blue_count * smallest_green_count
+        total += power
 
-        if isValid:
-            total += int(game_number)
-    
 print(total)
+

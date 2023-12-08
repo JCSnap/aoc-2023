eac1daa66c48942fde79aa85fb34443cb6948f7e
JCSnap <e0970167@u.nus.edu>
2023-12-08 17:17:51 +0800

    added day 8 part two

--- 8/day-eight.py
+++ 8/day-eight.py
@@ -1,8 +1,6 @@
-
-TARGET = "ZZZ"
+from math import gcd
 
 with open("day-eight.in", "r") as f:
-    # split by empty lines
     lines = f.read().strip().split('\n\n')
     sequence = lines[0]
     nodes = lines[1].split('\n')
@@ -16,11 +14,13 @@ with open("day-eight.in", "r") as f:
         right_dict[name] = right_child
 
     START = "AAA"
+    TARGET = "ZZZ"
     cur_name = START
     count = 0
     seq_len = len(sequence)
     cur_pos = 0
     while cur_name != TARGET:
+        # loop the sequence
         if cur_pos >= seq_len:
             cur_pos = 0
         cur_char = sequence[cur_pos]
@@ -32,3 +32,36 @@ with open("day-eight.in", "r") as f:
         count += 1
 
     print("Part 1:", count)
+
+    # Part 2
+    starts = []
+    # append all the nodes that ends with A into starts
+    for name in left_dict:
+        end = name[-1]
+        if end == "A":
+            starts.append(name)
+    
+    counts = []
+    for start in starts:
+        count = 0
+        cur_name = start
+        cur_pos = 0
+        while True:
+            if cur_pos >= seq_len:
+                cur_pos = 0
+            count += 1
+            cur_char = sequence[cur_pos]
+            if cur_char == "L":
+                cur_name = left_dict[cur_name]
+            else:
+                cur_name = right_dict[cur_name]
+            cur_pos += 1
+            if cur_name[-1] == "Z":
+                break
+        counts.append(count)
+
+    k = 1
+    for i in counts:
+        k = k * i // gcd(k, i)
+    print("Part 2:", k)
+        

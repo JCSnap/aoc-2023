ce28f72c098731067fdc053fcb1dc102c587ec8d
JCSnap <e0970167@u.nus.edu>
2023-12-09 15:10:10 +0800

    added day-nine part two

--- 9/day-nine.py
+++ 9/day-nine.py
@@ -1,16 +1,18 @@
 
 with open("day-nine.in", "r") as f:
-    total = 0
+    total_one = 0
+    total_two = 0
     for line in f:
         sequence = line.strip()
         numbers = sequence.split(' ')
-        print(numbers)
     
-        numbers = [int(num) for num in numbers]
+        numbers_original = [int(num) for num in numbers]
+        numbers = numbers_original.copy()
+
+        # Part 1
         last_nums = []
         all_zeros = False
         while not all_zeros:
-            print(numbers)
             all_zeros = True
             new_nums = []
             for i, num in enumerate(numbers):
@@ -21,10 +23,29 @@ with open("day-nine.in", "r") as f:
                     all_zeros = all_zeros and new_num == 0
                     new_nums.append(new_num)
             numbers = new_nums
-        # sum up everything in last_nums
-        print(last_nums)
-        total += sum(last_nums)
+        total_one += sum(last_nums)
 
-    print("Part 1:", total)
+        # Part 2
+        numbers = numbers_original.copy()
+        first_nums = []
+        all_zeros = False
+        while not all_zeros:
+            print(numbers)
+            all_zeros = True
+            new_nums = []
+            for i, num in enumerate(numbers):
+                if i == 0:
+                    first_nums.append(num)
+                if i > 0:
+                    new_num = numbers[i] - numbers[i-1]
+                    all_zeros = all_zeros and new_num == 0
+                    new_nums.append(new_num)
+            numbers = new_nums
+        
+        last = 0
+        for num in first_nums[::-1]:
+            last = num - last
+        total_two += last
 
-    # 939801360 too low
+    print("Part 1:", total_one)
+    print("Part 2:", total_two)
